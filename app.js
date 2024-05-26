c56994c7c049917afa275111c7cba7f3f302e9e7
Szymon <szekej51@gmail.com>
2024-05-26 20:39:22 +0200

    server is running

---
+++ app.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+const app = express();
+const PORT = 5000 || process.env.PORT;
+
+
+// temp engine
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejd');
+
+app.use('/', require('./server/routes/main'))
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+})

081dd060bf2955fdbac7b85cb12fb4ec320e1e88
Szymon <szekej51@gmail.com>
2024-05-27 20:52:53 +0200

    basic views, added base header and footter

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,7 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('', (req, res) => {
-    res.send("Hello Worls");
+    const locals = {
+        title: "Blog",
+        description: "Simple Blog App"
+    }
+    res.render('index', {locals});
+})
+
+router.get('/about', (req, res) => {
+    res.render('about');
 })
 
 module.exports = router;
